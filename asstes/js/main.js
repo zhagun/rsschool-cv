0abc1552dcc336aa4ea3d6bc4bedf2de300ce6f5
zhagun <zhagunpv@gmail.com>
2021-09-14 17:17:08 +0300

    feat: add section with adress and social

--- asstes/js/main.js
+++ asstes/js/main.js
@@ -42,3 +42,34 @@ function scrollActive(){
 }
 window.addEventListener('scroll', scrollActive)
 
+/*==================== SHOW SCROLL TOP ====================*/ 
+
+
+/*==================== DARK LIGHT THEME ====================*/ 
+
+
+/*==================== REDUCE THE SIZE AND PRINT ON AN A4 SHEET ====================*/ 
+
+
+/*==================== REMOVE THE SIZE WHEN THE CV IS DOWNLOADED ====================*/ 
+
+
+/*==================== GENERATE PDF ====================*/ 
+// PDF generated area
+
+
+// Html2pdf options
+
+
+// Function to call areaCv and Html2Pdf options 
+
+
+// When the button is clicked, it executes the three functions
+
+    // 1. The class .scale-cv is added to the body, where it reduces the size of the elements
+
+
+    // 2. The PDF is generated
+
+
+    // 3. The .scale-cv class is removed from the body after 5 seconds to return to normal size.
